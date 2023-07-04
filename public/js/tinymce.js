3f76f1ee157f73259845df220b100274a32b184a
danilovict2 <danilosindric@gmail.com>
2023-07-04 19:31:44 +0200

    Added a WYSIWYG Editor

---
+++ public/js/tinymce.js
@@ -0,0 +1,6 @@
+tinymce.init({
+    selector: 'textarea.tinymce',
+    plugins: 'link code lists',
+    toolbar: 'undo redo | fontfamily fontsize | bold italic | align | numlist bullist indent outdent | link',
+    menubar: false,
+});
